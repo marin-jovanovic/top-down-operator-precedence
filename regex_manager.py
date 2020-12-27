71eb7640b7eb138299279ffa0e206750063e1269
marin-jovanovic <marin.jovanovic12@gmail.com>
2020-12-27 15:58:10 +0100

    coed

--- regex_manager.py
+++ regex_manager.py
@@ -2,11 +2,10 @@ OUTPUT = list()
 ROW = 1
 
 
-# vraca true ako je ovo |
-# vraca false ako je ovo znak \|
+# ret true ako je ovo |
+# ret false ako je ovo \|
 def is_escaped_at_index(s, index):
 
-    # sve ispred indexa
     s = s[:index]
 
     count = 0
@@ -300,46 +299,23 @@ def get_first_token_index(s):
     else:
         return 0
 
-    # count = 0
-    # t_0 = ""
-    #
-    # i_1 = -1
-    # is_prev_escaper = False
-    # while i_1 != len(s) - 1:
-    #     i_1 += 1
-    #     token_1 = s[i_1]
-    #
-    #     print(i_1, token_1)
-    #     t_0 += token_1
-    #
-    #     if i_1 == 0:
-    #
-    #         if token_1 == "\\":
-    #             continue
-    #
-    #         elif token_1 not in ["("]:
-    #             count += 1
-    #
-    # print(t_0)
-    # return True
 
-
-def regex_driver(s):
+def regex_driver(s, start_index = 0, max_index = 0):
     print(["input", s])
     rules = list()
-    max_index = 0
-    start_index = 0
+    # max_index = 0
+    # start_index = 0
     prefix = "S_"
     accept_states = []
 
-    # zagrade na pocetku i kraju
-    # mice zagrade s pocetka i kraja dok postoje
-    # hendla svu logiku da mice samo ako ima smisla micat
-    t_0 = bracket_handler(s)
-    if t_0[0] == 0 and t_0[1] == len(s) - 1:
-        t_1 = s[t_0[0] + 1: t_0[1]]
-        print(["rekurzija, mako sam zagrade", t_1])
-        return regex_driver(t_1)
+    # # zagrade na pocetku i kraju
+    # # mice zagrade s pocetka i kraja dok postoje
+    # # hendla svu logiku da mice samo ako ima smisla micat
+    # t_0 = bracket_handler(s)
+    # if t_0[0] == 0 and t_0[1] == len(s) - 1:
+    #     t_1 = s[t_0[0] + 1: t_0[1]]
+    #     print(["rekurzija, mako sam zagrade", t_1])
+    #     return regex_driver(t_1)
 
     streams = split_by_separator(s)
     print("streams: " + str(streams))
@@ -357,26 +333,58 @@ def regex_driver(s):
             # tokens
             # \(, \), \+, \*
 
-            # # start of stream
-            # if i_0 == 0:
-
-            print(1, i_0, token_0)
-
-            # todo complete action actions
-
-
             source = prefix + str(start_index if i_0 == 0 else max_index)
             destination = prefix + str(max_index + 1)
             max_index += 1
 
+            a_states_buffer = []
+
             # action
             if token_0 == "(":
-                print("a")
                 t_1 = bracket_handler(s[i_0:])
                 t_0 = s[i_0 + t_1[0]: i_0 + t_1[1] + 1]
+
+                t_2 = s[i_0 + t_1[0] + 1: i_0 + t_1[1] - 1]
                 i_0 = t_1[1] + i_0
 
-                rules.append([source, t_0, destination])
+                # with brackets and +*
+                print(t_0)
+
+                # without brackets and *+
+                print(t_2)
+
+                new_streams = split_by_separator(t_2)
+
+                for i in new_streams:
+                    rules.append([source, "$", prefix + str(max_index)])
+                    max_index += 1
+
+                    new_rules, max_index, new_accept_states = regex_driver(i,
+                                                                           max_index - 1,
+                                                                           max_index - 1)
+
+                    print("max_index", max_index)
+
+                    [a_states_buffer.append(i) for i in new_accept_states]
+
+                    [rules.append(i) for i in new_rules]
+
+                destination = prefix + str(max_index)
+
+                for i in a_states_buffer:
+                    rules.append([i, "$", destination])
+
+                if s[t_1[1]] in ["*", "+"]:
+                    print("dodatni op")
+
+                    if s[t_1[1]] == "*":
+                        print("zvjezdica")
+
+                    elif s[t_1[1]] == "+":
+                        print("plusic")
+
+                    else:
+                        print("nista pametno")
 
             # token prefix handler
             elif token_0 == "\\":
@@ -389,29 +397,7 @@ def regex_driver(s):
 
             # token
             else:
-                print("c")
                 rules.append([source, token_0, destination])
-
-            # else:
-            #     if token_0 == "(":
-            #         t_1 = bracket_handler(s[i_0:])
-            #         t_0 = s[i_0 + t_1[0]: i_0 + t_1[1] + 1]
-            #         i_0 = t_1[1] + i_0
-            #
-            #         rules.append([prefix + str(max_index), t_0, prefix + str(max_index + 1)])
-            #         max_index += 1
-            #
-            #     elif token_0 == "\\":
-            #         i_0 += 1
-            #         token_0 = s[i_0]
-            #
-            # #         def token
-            #         rules.append([prefix + str(max_index), "\\" + token_0, prefix + str(max_index + 1)])
-            #         max_index += 1
-            #
-            #     else:
-            #         rules.append([prefix + str(max_index), token_0, prefix + str(max_index + 1)])
-            #         max_index += 1
 
         accept_states.append(max_index)
 
@@ -419,18 +405,17 @@ def regex_driver(s):
     print("accept states:")
     [print(prefix + str(i)) for i in accept_states]
     print("****************************************************************************************************")
-    return rules
+    return rules, max_index + 1, [(prefix + str(i)) for i in accept_states]
+    # , [(prefix + str(i)) for i in accept_states], max_index
 
 
-# todo
-#     specijalni znakovi
-#     +, *, (, ), [, ], .,
-if __name__ == '__main__':
+def run_tests():
 
-    # print(get_first_token_index("0\\123(3(1(2)+4)5)*67(2)89"))
-    #
-    # import sys
-    # sys.exit()
+
+    # todo " nije imao \, hendlaj sve te escapeove
+    regex_driver("(\\(|\\)|\\{|\\}|\\||\\*|\\\\|\\$|\\t|\\n|\\_|!|\"|#|%|&|\'|+|,|-|.|/|0|1|2|3|4|5|6|7|8|9|:|;|<|=|>|?"
+                 "|@|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|[|]|^|_|`|a|b|c|d|e|f|g|h|i|j|k|"
+                 "l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|~)")
 
     regex_driver("\\n")
 
@@ -446,14 +431,6 @@ if __name__ == '__main__':
 
     regex_driver("\\|#")
 
-    # todo " nije imao \, hendlaj sve te escapeove
-    regex_driver("(\\(|\\)|\\{|\\}|\\||\\*|\\\\|\\$|\\t|\\n|\\_|!|\"|#|%|&|\'|+|,|-|.|/|0|1|2|3|4|5|6|7|8|9|:|;|<|=|>|?"
-                 "|@|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|[|]|^|_|`|a|b|c|d|e|f|g|h|i|j|k|"
-                 "l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|~)")
-
-    regex_driver("((0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*|0x((0|1|2|3|4|5|6|7|8|9)|a|b|c|d|e|f|A|B|C|D"
-                 "|E|F)((0|1|2|3|4|5|6|7|8|9)|a|b|c|d|e|f|A|B|C|D|E|F)*)")
-
     regex_driver("-(\\t|\\n|\\_)*-")
 
     regex_driver("\\((\\t|\\n|\\_)*-")
@@ -467,9 +444,87 @@ if __name__ == '__main__':
     # regex_driver("ab|\\cd")
     # regex_driver("\\ab|\\cd")
 
-    regex_driver("(ab)+efd|(de)*def")
-    regex_driver("a|test|(dwa)har")
+    inp = "(ab)+efd|(de)*def"
+    rules, t, a_states = regex_driver(inp, 0, 0)
+    print("inp", inp)
+    [print(i) for i in rules]
+    [print(i) for i in a_states]
 
     # c = "a|test(dwakad)+djnaidjai"
     # t = bracket_handler(c)
     # print(c[t[0]:t[1] + 2])
+
+    inp = "a|test|(dwa)har"
+    rules, t, a_states = regex_driver(inp, 0, 0)
+    print("inp", inp)
+    [print(i) for i in rules]
+    [print(i) for i in a_states]
+
+
+    inp = "((0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*|0x((0|1|2|3|4|5|6|7|8|9)|a|b|c|d|e|f|A|B|C|D"\
+          "|E|F)((0|1|2|3|4|5|6|7|8|9)|a|b|c|d|e|f|A|B|C|D|E|F)*)"
+
+    # inp = "(0|1|2|3|4|5|6|7|8|9)"
+    rules, t, a_states = regex_driver(inp, 0, 0)
+    print("inp", inp)
+    [print(i) for i in rules]
+    [print(i) for i in a_states]
+
+    inp = "(0|1|2|3|4|5|6|7|8|9)"
+    rules, t, a_states = regex_driver(inp, 0, 0)
+    print("inp", inp)
+    [print(i) for i in rules]
+    [print(i) for i in a_states]
+
+    inp = "(0|1|2|3|4|5|6|7|8|9)*"
+    rules, t, a_states = regex_driver(inp, 0, 0)
+    print("inp", inp)
+
+    [print(i) for i in rules]
+    [print(i) for i in a_states]
+
+
+# todo
+#     specijalni znakovi
+#     +, *, (, ), [, ], .,
+if __name__ == '__main__':
+
+    # run_tests()
+    #
+    # s = "(0|1|2|3|4|5|6|7|8|9)*"
+    # t_0 = bracket_handler(s)
+    # print(s[t_0[1]])
+    #
+    # if s[t_0[1]] in ["*", "+"]:
+    #     print("dodatni op")
+
+    # rules, a_states, max_index = regex_driver("((0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*|0x((0|1|2|3|4|5|6|7|8|9)|a|b|c|d|"
+    #                                "e|f|A|B|C|D|E|F)((0|1|2|3|4|5|6|7|8|9)|a|b|c|d|e|f|A|B|C|D|E|F)*)")
+    #
+    # print(rules)
+    # print(a_states)
+    # print(max_index)
+
+    # s = "((0|1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*|0x((0|1|2|3|4|5|6|7|8|9)|a|b|c|d|e|f|A|B|C|D|E|F)" \
+    #     "((0|1|2|3|4|5|6|7|8|9)|a|b|c|d|e|f|A|B|C|D|E|F)*)"
+    # s = "(0|1|2|3|4|5|6|7|8|9)*"
+
+    # regex_driver(s, 0, 0)
+
+    #  \x1b[3;34;30m \x1b[0m
+
+    # rules, max_index, accept_states = regex_driver("m|n", 1, 3)
+
+    # run_tests()
+
+    inp = "a(mo|np)*d"
+    rules, t, a_states = regex_driver(inp, 0, 0)
+    print("inp", inp)
+    [print(i) for i in rules]
+    [print(i) for i in a_states]
+
+    # print(regex_driver("a(mo|np)*d", 0, 0))
+    # print("\33[31m " + str(regex_driver("a(mo|np)*d", 0, 0)) + " \033[0m")
+
+
+
